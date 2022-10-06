69354083033053c6fcf6bdd167b7ac49d67c8bae
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2022-10-06 09:58:03 -0700

    made some changes  to  db file

--- utils/db.js
+++ utils/db.js
@@ -1,43 +1,40 @@
 import mongoose from 'mongoose';
 
-const connection = {}
+const connection = {};
 
-const connect = async () => {
-    if (connection.isConnected) {
-        console.log('already connected');
-        return;
+async function connect() {
+  if (connection.isConnected) {
+    console.log('already connected');
+    return;
+  }
+  if (mongoose.connections.length > 0) {
+    connection.isConnected = mongoose.connections[0].readyState;
+    if (connection.isConnected === 1) {
+      console.log('use previous connection');
+      return;
     }
-
-    if (mongoose.connections.length > 0) {
-        connection.isConnected = mongoose.connections[0].readyState;
-        if (connection.isConnected === 1) { 
-            console.log('use previous connection');
-            return;
-        }
-        await mongoose.disconnect();
-    }
-
-    const db = mongoose.connect(process.env.MONGODB_URI);
-    console.log("new connection");
-    connection.isConnected = db.connections[0].readyState;
+    await mongoose.disconnect();
+  }
+  const db = await mongoose.connect(process.env.MONGODB_URI);
+  console.log('new connection');
+  connection.isConnected = db.connections[0].readyState;
 }
 
-const disconnect = async () => {
-    if (connection.isConnected) {
-        if (process.env.NODE_ENV === 'production') { 
-            await mongoose.disconnect();
-            connection.isConnected = false;
-        } else {
-            console.log('not disconnected');
-        }
+async function disconnect() {
+  if (connection.isConnected) {
+    if (process.env.NODE_ENV === 'production') {
+      await mongoose.disconnect();
+      connection.isConnected = false;
+    } else {
+      console.log('not disconnected');
     }
+  }
 }
- 
-const convertDocToObj = (doc) => {
-    doc._id = doc._id.toString();
-    doc.createdAt = doc.createdAt.toString();
-    doc.updatedAt = doc.updatedAt.toString();
-    return doc;
+function convertDocToObj(doc) {
+  doc._id = doc._id.toString();
+  doc.createdAt = doc.createdAt.toString();
+  doc.updatedAt = doc.updatedAt.toString();
+  return doc;
 }
 
 const db = { connect, disconnect, convertDocToObj };
