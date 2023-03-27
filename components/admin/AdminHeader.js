c33270d50fa1c8e8e04c7835a5793e0d1064b09c
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2023-03-27 03:46:02 -0700

    implement-admin-navbar

--- components/admin/AdminHeader.js
+++ components/admin/AdminHeader.js
@@ -1,9 +1,17 @@
 import { AiOutlineMenu } from "react-icons/ai";
+import { BsChatLeft } from "react-icons/bs";
+import { RiNotification3Line } from "react-icons/ri";
+import { MdKeyboardArrowDown } from "react-icons/md";
 import Tooltip from "@mui/material/Tooltip";
 import { useStateContext } from "../../context/ContextProvider";
-import { useEffect } from "react";
+import Cookies from "js-cookie";
+import { useContext, useEffect } from "react";
+import { Store } from "../../utils/Store";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { Menu } from "@headlessui/react";
 
-const NavButton = ({ title, placement, icon, customFunc, color, dotColor}) => (
+const NavButton = ({ title, placement, icon, customFunc, color, dotColor }) => (
   <Tooltip title={title} placement={placement}>
     <button
       type="button"
@@ -21,6 +29,12 @@ const NavButton = ({ title, placement, icon, customFunc, color, dotColor}) => (
 );
 
 function AdminHeader() {
+  const { dispatch } = useContext(Store);
+  // const { cart } = state;
+  // const [cartItemsCount, setCartItemsCount] = useState(0);
+
+  const { status, data: session } = useSession();
+
   const {
     activeMenu,
     setActiveMenu,
@@ -31,11 +45,21 @@ function AdminHeader() {
     setScreenSize,
   } = useStateContext();
 
+  // useEffect(() => {
+  //   setCartItemsCount(cart.cartItems.reduce((acc, i) => acc + i.quantity, 0));
+  // }, [cart.cartItems]);
+
+  const logoutClickHandler = () => {
+    Cookies.remove("cart");
+    dispatch({ type: "CART_RESET" });
+    signOut({ callbackUrl: "/login" });
+  };
+
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
     window.addEventListener("resize", handleResize);
     handleResize();
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, [setScreenSize]);
 
   useEffect(() => {
@@ -56,6 +80,53 @@ function AdminHeader() {
         placement="right-end"
         icon={<AiOutlineMenu color="#3b82f6" />}
       />
+      <div className="flex">
+        <NavButton
+          title="Chat"
+          // customFunc={() => handleClick("chat")}
+          icon={<BsChatLeft />}
+          color="#6b7280"
+          dotColor="#60a5fa"
+        />
+        <NavButton
+          title="Notifications"
+          // customFunc={() => handleClick("notification")}
+          icon={<RiNotification3Line />}
+          color="#6b7280"
+          dotColor="#60a5fa"
+        />
+        <Tooltip title="Profile">
+          {status === "loading" ? (
+            "Loading..."
+          ) : session.user.isAdmin ? (
+            <Menu
+              as="div"
+              className="flex relative justify-center items-center"
+            >
+              Hi,&nbsp;
+              <span className="text-blue-500">{session.user.name}</span>
+              <Menu.Button className="flex">
+                <MdKeyboardArrowDown className="text-gray-400 text-14 hover:bg-zinc-700 rounded-full" />
+              </Menu.Button>
+              <Menu.Items className="inline-block absolute right-3 top-10 origin-top-right w-38 bg-zinc-700 rounded-md shadow-lg p-2 w-28 text-center hover:border hover:border-zinc-500">
+                <Menu.Item>
+                  <a
+                    className="last:text-blue-500"
+                    href="#"
+                    onClick={logoutClickHandler}
+                  >
+                    Logout
+                  </a>
+                </Menu.Item>
+              </Menu.Items>
+            </Menu>
+          ) : (
+            <Link href="/login">
+              <a className="p-2 hover:text-indigo-500">Login</a>
+            </Link>
+          )}
+        </Tooltip>
+      </div>
     </div>
   );
 }
